baf8fa4e4a754f0b709e553583c30a67be69e6b3
Ryan Baermann <rcbaermann@gmail.com>
2019-09-01 15:38:22 -0700

    classified Big 0 time complexity

--- bubbleSort.js
+++ bubbleSort.js
@@ -18,3 +18,48 @@ function bubbleSort(arr) {
 }
 
 console.log(bubbleSort([6,4,2,4,1,9,5]));
+
+
+// o(n)
+
+function printArray(arr){
+    for(var i=0; i<arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+
+//0(1)
+
+function findNth(arr, n){
+    console.log(arr[n]);
+}
+
+//0(n)
+
+function halving(n){
+    var count = 0;
+    while(n > 1){
+        n = n/2;
+        count++;
+    }
+    return count;
+}
+
+// 0(n^2)
+
+function identityMatrix(n){
+    var matrix = [];
+    for(var i=0; i < n; i++){
+        var row = [];
+        for(var j=0; j < n; j++){
+            if(j == i){
+                row.push(1);
+            }
+            else{
+                row.push(0);
+            }
+        }
+        matrix.push(row);
+    }
+    return matrix;
+}
